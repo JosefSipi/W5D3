47fe16814eb66c66adcd4ede3cda75ca28885cad
akerpelm <akerpelm@u.rochester.edu>
2021-03-24 15:16:14 -0400

    Continue import_db

--- AA_Questions/import_db.sql
+++ AA_Questions/import_db.sql
@@ -1,8 +1,22 @@
+PRAGMA foreign_keys = ON;
+
+DROP IF EXISTS question_likes;
+DROP IF EXISTS replies;
+DROP IF EXISTS question_follows;
+DROP IF EXISTS questions;
+DROP IF EXISTS users;
+
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
 fname TEXT NOT NULL,
 lname TEXT NOT NULL
 ); 
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ('Joseph', 'Sipiorski'),
+    ('Alex', 'Kerpelman');
+
 
 CREATE TABLE questions (
 id INTEGER PRIMARY KEY,
@@ -13,6 +27,12 @@ author_id INTEGER NOT NULL,
 FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
+INSERT INTO 
+    questions(title, body, author_id)
+VALUES
+    ('Flex?', 'What is flex?', (SELECT id FROM users WHERE lname = 'Sipiorski')),
+    ('CSS?', 'What is CSS?', (SELECT id FROM users WHERE lname = 'Kerpelman'));
+
 CREATE TABLE question_follows (
 id INTEGER PRIMARY KEY,
 questions_id INTEGER NOT NULL,
@@ -22,13 +42,30 @@ FOREIGN KEY (questions_id) REFERENCES questions(id),
 FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
+INSERT INTO
+    quesetion_follows(questions_id, users_id)
+VALUES
+
+((SELECT id ...), (SELECT id ...)),
+
 CREATE TABLE replies (
 id INTEGER PRIMARY KEY,
 questions_id INTEGER NOT NULL,
-parent_id INTEGER NOT NULL,
+parent_id INTEGER   ,
+users_id INTEGER NOT NULL,
+body TEXT NOT NULL,
 
 FOREIGN KEY (questions_id) REFERENCES questions(id),
-FOREIGN KEY (parent_id) REFERENCES replies(id)
+FOREIGN KEY (parent_id) REFERENCES replies(id),
+FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
+CREATE TABLE question_likes (
+id INTEGER PRIMARY KEY,
+users_id INTEGER NOT NULL,
+questions_id INTEGER NOT NULL,
+
+FOREIGN KEY (users_id) REFERENCES users(id),
+FOREIGN KEY (questions_id) REFERENCES questions(id),
+)
 
